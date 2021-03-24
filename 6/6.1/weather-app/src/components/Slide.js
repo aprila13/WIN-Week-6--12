7a1c64f84737bc84b72d3b21002243733aa811bb
aprila13.name <aprilacraig1@gmail.com>
2021-03-24 11:17:09 -0400

    Updated slide time

--- 6/6.1/weather-app/src/components/Slide.js
+++ 6/6.1/weather-app/src/components/Slide.js
@@ -4,7 +4,7 @@ import Footer from '../components/Footer.js';
 
 class Slide extends React.Component {
   render() {
-    const html = '<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTjXuWWmlg1Hx5Aa7jNZ5XCZI7qIbGzyMtRVDzqSmGMIwKP51N38teA37WmA-FIx1cXZ6WTRrBSAZqs/embed?start=false&loop=false&delayms=600000" frameborder="0" width="960" height="660" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>';
+    const html = '<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTjXuWWmlg1Hx5Aa7jNZ5XCZI7qIbGzyMtRVDzqSmGMIwKP51N38teA37WmA-FIx1cXZ6WTRrBSAZqs/embed?start=false&loop=false" frameborder="0" width="960" height="660" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>';
     return <div>
       <div style={{ marginTop: '20px'}}>
         {ReactHtmlParser(html)}
