4179f95fe6be0c8125268eda5a452d0571d8f459
aprila13.name <aprilacraig1@gmail.com>
2021-03-25 09:53:36 -0400

    3/25 Commit

--- 6/6.4/redux-demo/src/index.js
+++ 6/6.4/redux-demo/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import StateCounter from './Components/StateCounter';
 import ReduxCounter from './Components/ReduxCounter';
 
