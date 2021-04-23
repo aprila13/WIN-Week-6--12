75b7263a36e3322c77517b9dc707f4ca2cf77dcf
aprila13.name <aprilacraig1@gmail.com>
2021-04-23 09:12:45 -0400

    4/23

---
+++ 10/Tic-Tac-Toe/src/com/tts/Main.java
@@ -0,0 +1,11 @@
+package com.tts;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        TicTacToeClass myGame = new TicTacToeClass();
+        myGame.displayBoard();
+    }
+}
